702ba00e35c4489bccf2e19daf6c01244247b915
bmbrost <bmbrost@gmail.com>
2015-04-18 18:52:20 -0600

    Simulate data for N-mixture model
    Simulate data according to N.mixture.mcmc.R

--- N.mixture.sim.R
+++ N.mixture.sim.R
@@ -1,29 +1,38 @@
 rm(list=ls())
 
+expit <- function(x) 1/(1+exp(-x))
+logit <- function(x) log(x/(1-p))
+
 ###
-### Simulate data according to model base.N.mixture.MCMC
+### Simulate data according to model N.mixture.MCMC
 ###
 
-m <- 1 # Number of sites
-J <- rep(500,m) # Number of replicate observations
+m <- 3 # Number of sites
+J <- 20 # Number of replicate observations
 lambda <- rep(150,m) # Intensity of Poisson distribution
-lambda <- c(150,200)
-p <- rep(0.75,m) # Detection probability
-p <- c(0.75,0.7)
+lambda <- rpois(m,100)
 N <- rpois(m,lambda) # True population size 
-Y <- t(sapply(1:m,function(x) rbinom(J[x],N[x],p[x]))) # Observed counts
 
+W <- array(1,dim=c(J,2,m)) # Design matrix for detection probability
+# hist(rgamma(1000,12,2))
+W[,2,] <- rgamma(m*J,12,2) # Detection covariate, e.g., flight duration
+hist(W[,2,])
+
+alpha <- matrix(c(-5,0.75),2,1) # Detection parameters
+curve(expit(cbind(1,x)%*%alpha),xlim=c(0,max(W[,2,])),add=FALSE)
+p <- apply(W,3,function(x) expit(x%*%alpha)) # Detection probability
+Y <- t(sapply(1:m,function(x) rbinom(J,N[x],p[,x]))) # Observed counts
 
 ###
 ### Fit model using MCMC algorithm
 ###
 
-source("base.N.mixture.MCMC.R")
+source("N.mixture.MCMC.R")
 # hist(rgamma(1000,5,0.01))
-out1 <- base.N.mixture.MCMC(Y,priors=list(a=1,b=1,r=5,q=0.001),tune=list(N=5),n.mcmc=20000)
+out1 <- N.mixture.MCMC(Y,W,priors=list(r=5,q=0.001,tau=2),tune=list(N=5,alpha=0.01),n.mcmc=10000)
 
 matplot(t(out1$N),type="l");abline(h=N,col=2,lty=2)
-matplot(t(out1$p),type="l");abline(h=p,col=2,lty=2)
+matplot(out1$alpha,type="l");abline(h=alpha,col=2,lty=2)
 matplot(t(out1$lambda),type="l");abline(h=lambda,col=2,lty=2)
 
 mean(out1$N[-(1:100)])
