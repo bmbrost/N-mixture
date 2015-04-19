5f85938113accdfc9fc7220c619af5f014672d6b
bmbrost <bmbrost@gmail.com>
2015-04-18 18:51:27 -0600

    MCMC algorithm for N-mixture model
    Fit N-mixture model for multiple sites. Detection probability (p) is
    modeled as a function of covariates. Poisson intensity (lambda) can
    vary by site but is not modeled as a function of covariates.

--- N.mixture.MCMC.R
+++ N.mixture.MCMC.R
@@ -1,20 +1,21 @@
-base.N.mixture.MCMC <- function(Y,priors=list(a=1,b=1,r=15,q=0.1),tune=list(N=5),n.mcmc=1000){
-  
+N.mixture.MCMC <- function(Y,W,priors=list(r=15,q=0.1,tau=2),tune=list(N=5,alpha=0.01),n.mcmc=1000){
+
   ###
   ### Brian M. Brost (17APR2015)
   ###
-  ### Base N-mixture model for multiple sites (Royle 2004)
+  ### N-mixture model for multiple sites with detection covariates (Royle 2004)
   ###
   ### Model statement:
-  ### Y[i,j]~Binom(N[j],p)
+  ### Y[i,j]~Binom(N[j],p[i,j])
   ### N[j]~Pois(lambda[j])
   ### lambda[j]~Gamma(r,q)
-  ### p~beta(a,b)
+  ### logit(p[i,j])~W[j,,i]%*%alpha
   ###
   ### Function arguments: 
   ### Y=m*J matrix, where m is the number of sites and J is the maximum number of
   ###   observations across all sites
-  ### priors=parameters of prior distributionsfor p and lambda
+  ### W=J*length(alpha)*m array; each 'slice' of W is a design matrix for model on detection
+  ### priors=parameters of prior distributions for lambda and alpha
   ###
     
   ###
@@ -22,14 +23,17 @@ base.N.mixture.MCMC <- function(Y,priors=list(a=1,b=1,r=15,q=0.1),tune=list(N=5)
   ###
 
   #   browser()
-  m <- nrow(Y) #Number of sites
-  y <- rowSums(Y) #Total of observed counts by site
+  m <- nrow(Y) # Number of sites
+  J <- ncol(Y) # Number of replicate counts
+  y <- rowSums(Y) # Total of observed counts by site
   y.min <- apply(Y,1,min)
-  keep <- 0
+  keep <- list(N=0,alpha=0)
+  qW <- ncol(W)
+  W.mat <- apply(W,2,I) # Convert W from 3-D array to 2-D matrix
   
   N.save <- matrix(0,m,n.mcmc)
   lambda.save <- matrix(0,m,n.mcmc)
-  p.save <- matrix(0,m,n.mcmc)
+  alpha.save <- matrix(0,n.mcmc,qW)
   
   ###
   ###  Priors and starting values 
@@ -37,6 +41,8 @@ base.N.mixture.MCMC <- function(Y,priors=list(a=1,b=1,r=15,q=0.1),tune=list(N=5)
   
   N.tune <- seq(-1*tune$N,tune$N,1)
 
+  alpha <- rnorm(qW,0,priors$tau)
+  alpha <- c(-5,0.75)
   N <- apply(Y,1,max)+1
   lambda <- N
   
@@ -48,13 +54,20 @@ base.N.mixture.MCMC <- function(Y,priors=list(a=1,b=1,r=15,q=0.1),tune=list(N=5)
     if(k%%1000==0) cat(k,"");flush.console()
         
     ###
-    ###  Sample p 
+    ###  Sample alpha 
     ###
 # browser()
-  Y  
-  p <- rbeta(m,y+priors$a,sapply(1:m,function(x) sum(N[x]-Y[x,]))+priors$b)
-# p <- rbeta(m,y+priors$a,sum(N-Y))+priors$b)
-
+  
+    alpha.star <- rnorm(qW,alpha,tune$alpha*I(qW))
+    p.star <- expit(W.mat%*%alpha.star)
+    N.tmp <- rep(N,each=J)  
+    mh.star.alpha <- sum(dbinom(c(t(Y)),N.tmp,p.star,log=TRUE))+sum(dnorm(alpha.star,0,priors$tau,log=TRUE))
+    mh.0.alpha <- sum(dbinom(c(t(Y)),N.tmp,p,log=TRUE))+sum(dnorm(alpha,0,priors$tau,log=TRUE))
+    if(exp(mh.star.alpha-mh.0.alpha)>runif(1)){
+      alpha <- alpha.star
+      p <- expit(W.mat%*%alpha)
+      keep$alpha <- keep$alpha+1
+    }
 
     ###
     ###  Sample N 
@@ -65,24 +78,18 @@ base.N.mixture.MCMC <- function(Y,priors=list(a=1,b=1,r=15,q=0.1),tune=list(N=5)
 
     N.star <- N + sample(N.tune,m,replace=TRUE)
     idx <- which(N.star>=0 & N.star>y.min)
+    N.star.tmp <- rep(N.star[idx],each=J)
+    N.tmp <- rep(N[idx],each=J)
     n.tmp <- length(idx)
 #     browser()
-    if(length(idx)>0){
-      mh.star.N <- rowSums(matrix(dbinom(Y[idx,],N.star[idx],p[idx],log=TRUE),n.tmp,J))+dpois(N.star[idx],lambda[idx],log=TRUE)
-      mh.0.N <- rowSums(matrix(dbinom(Y[idx,],N[idx],p[idx],log=TRUE),n.tmp,J))+dpois(N[idx],lambda[idx],log=TRUE)  
+    if(n.tmp>0){
+      mh.star.N <- sum(dbinom(c(t(Y[idx,])),N.star.tmp,p,log=TRUE))+dpois(N.star[idx],lambda[idx],log=TRUE)
+      mh.0.N <- sum(dbinom(c(t(Y[idx,])),N.tmp,p,log=TRUE))+dpois(N[idx],lambda[idx],log=TRUE)  
       idx <- idx[exp(mh.star.N-mh.0.N)>runif(n.tmp)]
       N[idx] <- N.star[idx]
-      keep <- keep+length(idx)
-    }
+      keep$N <- keep$N+length(idx)
+    }      
 
-#   if(length(idx)>0){
-#   mh.star.N <- sum(dbinom(Y,N.star,p,log=TRUE))+dpois(N.star,lambda,log=TRUE)
-#   mh.0.N <- sum(dbinom(Y,N,p,log=TRUE))+dpois(N,lambda,log=TRUE)
-#   if(exp(mh.star.N-mh.0.N)>runif(1)){
-#     N <- N.star
-#     keep <- keep+1
-#   }
-# }
 
     ####
     ####  Sample lambda 
@@ -94,7 +101,7 @@ base.N.mixture.MCMC <- function(Y,priors=list(a=1,b=1,r=15,q=0.1),tune=list(N=5)
     ####  Save Samples 
     ####
     
-    p.save[,k] <- p
+    alpha.save[k,] <- alpha
     N.save[,k] <- N
     lambda.save[,k] <- lambda
   }
@@ -102,8 +109,10 @@ base.N.mixture.MCMC <- function(Y,priors=list(a=1,b=1,r=15,q=0.1),tune=list(N=5)
   ####
   ####  Write Output 
   ####
-
-  cat("\n")
-  cat(paste("Acceptance rate:",keep/(n.mcmc*m)))  
-  list(p=p.save,N=N.save,lambda=lambda.save,keep=keep/(n.mcmc*m),n.mcmc=n.mcmc)
+  
+  keep$N <- keep$N/(n.mcmc*m)
+  keep$alpha <- keep$alpha/n.mcmc
+  cat(paste("\nN acceptance rate:",keep$N))  
+  cat(paste("\nalpha acceptance rate:",keep$alpha))  
+  list(alpha=alpha.save,N=N.save,lambda=lambda.save,keep=keep,n.mcmc=n.mcmc)
 }
