a1a3c625194aa7b3cfd163e1ef3e94052337c7b5
bmbrost <bmbrost@gmail.com>
2015-04-26 09:56:10 -0600

    Add trend estimation to N.mixture.mcmc

--- N.mixture.trend.MCMC.R
+++ N.mixture.trend.MCMC.R
@@ -1,4 +1,4 @@
-N.mixture.trend.MCMC <- function(Y,W,priors=list(r=15,q=0.1,tau=2,sigma=2),tune=list(N=5,alpha=0.01,theta=0.01),n.mcmc=1000){
+N.mixture.trend.MCMC <- function(Y,W,priors,tune,start,n.mcmc=1000){
 
   ###
   ### Brian M. Brost (17APR2015)
@@ -24,17 +24,19 @@ N.mixture.trend.MCMC <- function(Y,W,priors=list(r=15,q=0.1,tau=2,sigma=2),tune=
   ###  Setup Variables 
   ###
 
-  #   browser()
-  T <- nrow(Y) # Number of sites
-  J <- ncol(Y) # Number of replicate counts
-  y <- rowSums(Y) # Total of observed counts by site
-  y.min <- apply(Y,1,min)
-  keep <- list(N=0,alpha=0)
+#     browser()
+  T <- nrow(Y) # Number of time periods
+  m <- apply(Y,1,function(x) sum(!is.na(x))) # Number of observations per time period
+  p.idx <- cbind(cumsum(m)-m+1,cumsum(m)) # Index for matching records in Y to p
+  y <- rowSums(Y) # Total of observed counts by time period
+  y.max <- apply(Y,1,max) # Maximum count per time period
+  
+  keep <- list(N=0,alpha=0,theta=0)
   qW <- ncol(W)
   W.mat <- apply(W,2,I) # Convert W from 3-D array to 2-D matrix
   
-  N.save <- matrix(0,m,n.mcmc)
-  lambda.save <- matrix(0,m,n.mcmc)
+  N.save <- matrix(0,n.mcmc,T)
+  lambda.save <- numeric(n.mcmc)
   alpha.save <- matrix(0,n.mcmc,qW)
   theta.save <- numeric(n.mcmc)
   
@@ -43,11 +45,16 @@ N.mixture.trend.MCMC <- function(Y,W,priors=list(r=15,q=0.1,tau=2,sigma=2),tune=
   ###
   
   N.tune <- seq(-1*tune$N,tune$N,1)
-
   alpha <- rnorm(qW,0,priors$tau)
-  alpha <- c(-5,0.75)
+  theta <- 0.987 # Population growth rate
   N <- apply(Y,1,max)+1
-  lambda <- N
+  lambda <- N[1]
+  
+  alpha <- start$alpha
+  N <- start$N
+  lambda <- start$lambda
+  theta <- start$theta
+  p <- expit(W.mat%*%alpha)
   
   ###
   ###  Begin MCMC loop
@@ -63,7 +70,7 @@ N.mixture.trend.MCMC <- function(Y,W,priors=list(r=15,q=0.1,tau=2,sigma=2),tune=
   
     alpha.star <- rnorm(qW,alpha,tune$alpha*I(qW))
     p.star <- expit(W.mat%*%alpha.star)
-    N.tmp <- rep(N,each=J)  
+    N.tmp <- c(sapply(1:T,function(x) rep(N[x],m[x])))    
     mh.star.alpha <- sum(dbinom(c(t(Y)),N.tmp,p.star,log=TRUE))+sum(dnorm(alpha.star,0,priors$tau,log=TRUE))
     mh.0.alpha <- sum(dbinom(c(t(Y)),N.tmp,p,log=TRUE))+sum(dnorm(alpha,0,priors$tau,log=TRUE))
     if(exp(mh.star.alpha-mh.0.alpha)>runif(1)){
@@ -73,61 +80,95 @@ N.mixture.trend.MCMC <- function(Y,W,priors=list(r=15,q=0.1,tau=2,sigma=2),tune=
     }
 
     ###
-    ###  Sample N for t=1 
+    ###  Sample N[1] (i.e., abundance for t=1)
     ###    
     
-    N.star <- N[1] + sample(N.tune,1,replace=TRUE)
-    if(N.star>=0 & N.star>y.min[1]){
-      mh.star.N <- sum(dbinom(Y[1,],N.star,p,log=TRUE))+dpois(N.star[1],lambda[1],log=TRUE)
-      mh.0.N <- sum(dbinom(Y[1,],N,p,log=TRUE))+dpois(N[1],lambda[1],log=TRUE)  
-      if(exp(mh.star.N-mh.0.N)>runif(1){
+    N.star <- N[1] + sample(N.tune,1)
+    if(N.star>y.max[1]){ # Update N[1] only if N.star>y.max[1]
+      p.tmp <- p[p.idx[1,1]:p.idx[1,2]] # Detection probabilities for t=1
+      mh.star.N <- sum(dbinom(Y[1,],N.star,p.tmp,log=TRUE))+dpois(N.star,lambda,log=TRUE)
+      mh.0.N <- sum(dbinom(Y[1,],N[1],p.tmp,log=TRUE))+dpois(N[1],lambda,log=TRUE)  
+      if(exp(mh.star.N-mh.0.N)>runif(1)){
         N[1] <- N.star
         keep$N <- keep$N+1
       }    
     }      
 
     ###
-    ###  Sample N for t=2:T 
+    ###  Sample N[2:(t-1)] (i.e., abundance for t=2:(T-1))
+    ###   
+    
+    for(t in 2:(T-1)){
+      N.star <- N[t] + sample(N.tune,1)
+      if(N.star>y.max[t]){ # Update N[t] only if N.star>y.max[t]
+        p.tmp <- p[p.idx[t,1]:p.idx[t,2]] # Detection probabilities for t=t
+        mh.star.N <- sum(dbinom(Y[t,],N.star,p.tmp,log=TRUE))+
+          dpois(N.star,theta*N[t-1],log=TRUE)+dpois(N[t+1],theta*N.star,log=TRUE)
+        mh.0.N <- sum(dbinom(Y[t,],N[t],p.tmp,log=TRUE))+
+          dpois(N[t],theta*N[t-1],log=TRUE)+dpois(N[t+1],theta*N[t],log=TRUE)
+        if(exp(mh.star.N-mh.0.N)>runif(1)){
+           N[t] <- N.star
+          keep$N <- keep$N+1
+        }    
+      }      
+    }
+    
+    
+    ###
+    ###  Sample N[T] (i.e., abundance for t=T)
     ###    
-
-    N.star <- N + sample(N.tune,m,replace=TRUE)
-
-    idx <- which(N.star>=0 & N.star>y.min)
-    N.star.tmp <- rep(N.star[idx],each=J)
-    N.tmp <- rep(N[idx],each=J)
-    n.tmp <- length(idx)
-    #     browser()
-    if(n.tmp>0){
-      mh.star.N <- sum(dbinom(c(t(Y[idx,])),N.star.tmp,p,log=TRUE))+dpois(N.star[idx],lambda[idx],log=TRUE)
-      mh.0.N <- sum(dbinom(c(t(Y[idx,])),N.tmp,p,log=TRUE))+dpois(N[idx],lambda[idx],log=TRUE)  
-      idx <- idx[exp(mh.star.N-mh.0.N)>runif(n.tmp)]
-      N[idx] <- N.star[idx]
-      keep$N <- keep$N+length(idx)
+    
+    N.star <- N[T] + sample(N.tune,1)
+    if(N.star>y.max[T]){ # Update N[T] only if N.star>y.max[T]
+      p.tmp <- p[p.idx[T,1]:p.idx[T,2]] # Detection probabilities for t=T
+      mh.star.N <- sum(dbinom(Y[T,],N.star,p.tmp,log=TRUE))+dpois(N.star,theta*N[T-1],log=TRUE)
+      mh.0.N <- sum(dbinom(Y[T,],N[T],p.tmp,log=TRUE))+dpois(N[T],theta*N[T-1],log=TRUE)  
+      if(exp(mh.star.N-mh.0.N)>runif(1)){
+        N[T] <- N.star
+        keep$N <- keep$N+1
+      }    
     }      
+    
+    
+    ###
+    ###  Sample lambda  
+    ###
+    
+    lambda <- rgamma(1,shape=N[1]+priors$r,rate=1+priors$q)
+    
+    
+    ###
+    ###  Sample theta
+    ###
+#     browser()
+    theta.star <- rnorm(1,theta,tune$theta)
+    mh.star.theta <- sum(dpois(N[-1],theta.star*N[-T],log=TRUE))+dnorm(theta.star,1,priors$sigma,log=TRUE)
+    mh.0.theta <- sum(dpois(N[-1],theta*N[-T],log=TRUE))+dnorm(theta,1,priors$sigma,log=TRUE)
+    if(exp(mh.star.theta-mh.0.theta)>runif(1)){
+      theta <- theta.star
+      keep$theta <- keep$theta+1
+    }
 
 
-    ####
-    ####  Sample lambda 
-    ####
-    
-    lambda <- rgamma(m,shape=N+priors$r,rate=1+priors$q)
-    
-    ####
-    ####  Save Samples 
-    ####
+    ###
+    ###  Save Samples 
+    ###
     
     alpha.save[k,] <- alpha
-    N.save[,k] <- N
-    lambda.save[,k] <- lambda
+    N.save[k,] <- N
+    lambda.save[k] <- lambda
+    theta.save[k] <- theta
   }
   
-  ####
-  ####  Write Output 
-  ####
+  ###
+  ###  Write Output 
+  ###
   
-  keep$N <- keep$N/(n.mcmc*m)
+  keep$N <- keep$N/(n.mcmc*T)
   keep$alpha <- keep$alpha/n.mcmc
-  cat(paste("\nN acceptance rate:",keep$N))  
-  cat(paste("\nalpha acceptance rate:",keep$alpha))  
-  list(alpha=alpha.save,N=N.save,lambda=lambda.save,keep=keep,n.mcmc=n.mcmc)
+  keep$theta <- keep$theta/n.mcmc
+  cat(paste("\nN acceptance rate:",round(keep$N,2)))  
+  cat(paste("\nalpha acceptance rate:",round(keep$alpha,2)))
+  cat(paste("\ntheta acceptance rate:",round(keep$theta,2)))  
+  list(alpha=alpha.save,N=N.save,lambda=lambda.save,theta=theta.save,keep=keep,n.mcmc=n.mcmc)
 }
--- N.mixture.trend.sim.R
+++ N.mixture.trend.sim.R
@@ -4,65 +4,56 @@ expit <- function(x) 1/(1+exp(-x))
 logit <- function(x) log(x/(1-p))
 
 ###
-### Simulate data according to model N.mixture.MCMC
+### Simulate data according to model N.mixture.trend.MCMC
 ###
 
-m <- 1 # Number of sites
-T <- 20 # Number of years of data
-J <- 20 # Number of replicate observations
-lambda <- numeric(T)
-lambda[1] <- 1200 # Intensity of Poisson distribution for t=1
+T <- 8 # Number of years of data
+m <- 10 # Number of replicate observations
+lambda <- 1200 # Intensity of Poisson distribution for t=1
 theta <- 0.987 # Population growth rate
-z <- log(lambda)
-
-for(i in 2:T) lambda[i] <- theta*lambda[i-1] # Intensity with linear trend
-plot(lambda,type="l")
-for(i in 2:T) z[i] <- log(theta)+z[i-1] # Log-transformed intensity with linear trend
-plot(z,type="l")
-
-N <- rpois(T,exp(z)) # True population size
+# z <- log(lambda)
+  
+# Simulate true abundance with trend
+N <- numeric(T)
+N[1] <- rpois(1,lambda) # Initial population size at t=1
+for(i in 2:T) N[i] <- rpois(1,theta*N[i-1]) # Intensity with linear trend
 plot(N,type="l")
 
-W <- array(1,dim=c(J,2,T)) # Design matrix for detection probability
+W <- array(1,dim=c(m,2,T)) # Design matrix for detection probability
 # hist(rgamma(1000,12,2))
-W[,2,] <- rgamma(J*T,12,2) # Detection covariate, e.g., flight duration
+W[,2,] <- rgamma(m*T,12,2) # Detection covariate, e.g., flight duration
 hist(W[,2,])
 
 alpha <- matrix(c(-5,0.75),2,1) # Detection parameters
-curve(expit(cbind(1,x)%*%alpha),xlim=c(0,max(W[,2,])),add=FALSE)
-p <- apply(W,3,function(x) expit(x%*%alpha)) # Detection probability
-Y <- t(sapply(1:T,function(x) rbinom(J,N[x],p[,x]))) # Observed counts, T*J matrix
+curve(expit(cbind(1,x)%*%alpha),xlim=c(0,max(W[,2,])),add=FALSE) # Response curve for p
+p <- t(apply(W,3,function(x) expit(x%*%alpha))) # Detection probability
+Y <- t(sapply(1:T,function(x) rbinom(m,N[x],p[x,]))) # Observed counts, T*m matrix
 
+# Compare observed versus true detection probability
+p.hat <- t(sapply(1:T,function(x) Y[x,]/N[x])) # Observed detection probability
+matplot(p.hat,pch=19,cex=1,col=rgb(0,0,0,0.25),xlab="Time")
+matplot(p,pch=19,cex=0.5,col=rgb(1,0,0,0.25),add=TRUE) # True detection probability
+
+# Plot true true abundance and observed counts
+plot(N,type="l",ylim=c(min(Y),max(N))) # True abundance
+matplot(Y,pch=19,cex=0.5,col=rgb(1,0,0,0.25),add=TRUE) # Observed counts
 
 ###
 ### Fit model using MCMC algorithm
 ###
 
 source("N.mixture.trend.MCMC.R")
-# hist(rgamma(1000,5,0.01))
+# hist(rgamma(1000,2,0.001),breaks=100)
 
-priors <- list(r=5,q=0.001,tau=2,sigma=2)
-tune <- list(N=5,alpha=0.01,theta=0.01)
-out1 <- N.mixture.trend.MCMC(Y,W,priors=priors,tune=tune,n.mcmc=10000)
+priors <- list(r=2,q=0.001,tau=2,sigma=2)
+tune <- list(N=30,alpha=0.003,theta=0.05)
+start <- list(N=N,alpha=alpha,lambda=lambda,theta=theta)
+out1 <- N.mixture.trend.MCMC(Y,W,priors,tune,start,n.mcmc=3000)
 
-matplot(t(out1$N),type="l");abline(h=N,col=2,lty=2)
-matplot(out1$alpha,type="l");abline(h=alpha,col=2,lty=2)
-matplot(t(out1$lambda),type="l");abline(h=lambda,col=2,lty=2)
-
-mean(out1$N[-(1:100)])
-mean(out1$lambda[-(1:100)])
-mean(out1$p[-(1:100)])
-
-
-###
-### Fit model using R package 'unmarked'
-###
-
-library(unmarked)
-visitMat <- matrix(as.character(1:J), m, J, byrow=TRUE)
-umf <- unmarkedFramePCount(y=Y, obsCovs=list(visit=visitMat))
-summary(umf)
-
-fm1 <- pcount(~1 ~1, umf, K=200)
-fm1
-plogis(coef(fm1, type="det")) # Should be close to p
+matplot(out1$alpha[,],type="l");abline(h=alpha,col=2,lty=2)
+apply(out1$alpha,2,mean);apply(out1$alpha,2,quantile,c(0.025,0.975))
+matplot(out1$N,type="l",col=1:T,lty=1);abline(h=N,col=1:T,lty=2)
+apply(out1$N,2,mean)
+N
+plot(out1$lambda,type="l");abline(h=lambda,col=2,lty=2)
+plot(out1$theta,type="l");abline(h=theta,col=2,lty=2)
