cf1bfaad3af99567bf5eac42bbfa9767823f3bfc
bmbrost <bmbrost@gmail.com>
2015-04-29 22:49:46 -0600

    update N.mixture.mcmc to include random effect

---
+++ N.mixture.re.MCMC.R
@@ -0,0 +1,173 @@
+N.mixture.re.MCMC <- function(Y,W,priors,tune,start,n.mcmc=1000){
+
+  ###
+  ### Brian M. Brost (25APR2015)
+  ###
+  ### N-mixture model for multiple sites with detection modeled by covariates and random effect on alpha
+  ###
+  ### Model statement: (i indexes site, j indexes observation)
+  ### Y[i,j]~Binom(N[i],p[i,j])
+  ### N[i]~Pois(lambda[i])
+  ### lambda[i]~Gamma(r,q)
+  ### logit(p[i,j])~W[j,,i]%*%alpha
+  ### alpha[t] ~ N(mu_alpha,tau^2*I)
+  ### mu_alpha~N(0,zeta^2*I)
+  ###
+  ### Function arguments: 
+  ### Y=m*J matrix, where m is the number of sites and J is the maximum number of
+  ###   observations across all sites
+  ### W=J*length(alpha)*m array; each 'slice' of W is a design matrix for detection model at a site
+  ### priors=parameters of prior distributions for lambda and alpha
+  ### tune=tuning parameters for N and alpha
+  ### start=starting values for N,lambda, and alpha
+  ###
+    
+  ###
+  ###  Libraries and subroutines
+  ###
+  
+  expit <- function(x) 1/(1+exp(-x))
+  logit <- function(x) log(x/(1-p))
+  
+  
+  ###
+  ###  Setup Variables 
+  ###
+
+# browser()
+  m <- nrow(Y) # Number of sites
+  qY <- ncol(Y)
+  J <- apply(Y,1,function(x) sum(!is.na(x))) # Number of observations per site
+  y.max <- apply(Y,1,max,na.rm=TRUE)
+  qW <- ifelse(is.null(ncol(W)),1,ncol(W))
+  keep <- list(N=0,alpha=0,tau=0)
+
+  N.save <- matrix(0,n.mcmc,m)
+  lambda.save <- matrix(0,n.mcmc,m)
+  mu.alpha.save <- matrix(0,n.mcmc,qW)
+  alpha.save <- array(0,dim=c(n.mcmc,qW,m))  
+  tau.save <- numeric(n.mcmc)  
+
+  ###
+  ###  Tuning and starting values 
+  ###
+  
+  N.tune <- seq(-1*tune$N,tune$N,1)
+
+  alpha <- start$alpha
+  N <- start$N
+  lambda <- start$lambda
+  mu.alpha <- start$mu.alpha  
+  tau <- start$tau
+
+  alpha.tmp <- sapply(1:qW,function(x) rep(alpha[,x],each=qY))
+  p <- matrix(expit(apply(W*alpha.tmp,1,sum)),m,qY,byrow=TRUE)
+  
+
+  ###
+  ###  Begin MCMC loop
+  ###
+  
+  for(k in 1:n.mcmc){
+    if(k%%1000==0) cat(k,"");flush.console()
+        
+    ###
+    ###  Sample lambda 
+    ###
+    
+    lambda <- rgamma(m,shape=N+priors$r,rate=1+priors$q)
+    
+    
+    ###
+    ###  Sample alpha[t] 
+    ###
+    
+    alpha.star <- matrix(rnorm(qW*m,alpha,tune$alpha),m,qW)
+    alpha.star.tmp <- sapply(1:qW,function(x) rep(alpha.star[,x],each=qY))
+    p.star <- matrix(expit(apply(W*alpha.star.tmp,1,sum)),m,qY,byrow=TRUE)
+    for(i in 1:m){
+      mh.star.alpha <- sum(dbinom(Y[i,],N[i],p.star[i,],log=TRUE),na.rm=TRUE)+
+        sum(dnorm(alpha.star[i,],mu.alpha,tau,log=TRUE))
+      mh.0.alpha <- sum(dbinom(Y[i,],N[i],p[i,],log=TRUE),na.rm=TRUE)+
+        sum(dnorm(alpha[i,],mu.alpha,tau,log=TRUE))
+      if(exp(mh.star.alpha-mh.0.alpha)>runif(1)){
+        alpha[i,] <- alpha.star[i,]
+        keep$alpha <- keep$alpha+1
+      }
+    }
+    alpha.tmp <- sapply(1:qW,function(x) rep(alpha[,x],each=qY))
+    p <- matrix(expit(apply(W*alpha.tmp,1,sum)),m,qY,byrow=TRUE)
+
+    ###
+    ###  Sample tau
+    ###
+
+    tau.star <- rnorm(1,tau,tune$tau)
+    if(tau.star>priors$a&tau.star<priors$b){
+      mh.star.tau <- sum(dnorm(t(alpha),mu.alpha,tau.star,log=TRUE),na.rm=TRUE)
+      mh.0.tau <- sum(dnorm(t(alpha),mu.alpha,tau,log=TRUE),na.rm=TRUE)
+      if(exp(mh.star.tau-mh.0.tau)>runif(1)){
+        tau <- tau.star
+        keep$tau <- keep$tau+1
+      } 
+    }
+    
+    
+    ###
+    ###  Sample mu.alpha 
+    ###
+#     browser()
+    
+    A <- m*solve(tau^2*diag(qW))+solve(zeta^2*diag(qW))
+    b <- rowSums(apply(alpha,1,function(x) x%*%solve(tau^2*diag(qW))))+solve(zeta^2*diag(qW))
+#       +priors$mu.0%*%solve(zeta^2*diag(qW))
+    mu.alpha <- rnorm(2,solve(A)%*%t(b),diag(solve(A)))
+#     mu.alpha <- start$mu.alpha
+    
+#     alpha[1,]%*%solve(tau^2*diag(qW))  
+#   
+#     exp(0.5*(2*3))*exp(0.5*(2*4))
+#     exp(0.5*(2*(3+4)))
+#   m 
+#   m*t(mu.alpha)%*%solve(tau^2*diag(2))%*%mu.alpha
+#   t(mu.alpha)%*%(m*solve(tau^2*diag(2)))%*%mu.alpha
+#     
+    ###
+    ###  Sample N 
+    ###
+    
+    N.star <- N + sample(N.tune,m,replace=TRUE)    
+    idx <- which(N.star>y.max)
+      for(i in idx){
+        mh.star.N <- sum(dbinom(Y[i,],N.star[i],p[i,],log=TRUE),na.rm=TRUE)+dpois(N.star[i],lambda[i],log=TRUE)
+        mh.0.N <- sum(dbinom(Y[i,],N[i],p[i,],log=TRUE),na.rm=TRUE)+dpois(N[i],lambda[i],log=TRUE)  
+        if(exp(mh.star.N-mh.0.N)>runif(1)){
+          N[i] <- N.star[i]
+          keep$N <- keep$N+1
+        }
+      }  
+    
+    
+    ###
+    ###  Save Samples 
+    ###
+    
+    alpha.save[k,,] <- t(alpha)
+    mu.alpha.save[k,] <- mu.alpha
+    N.save[k,] <- N
+    lambda.save[k,] <- lambda
+    tau.save[k] <- tau
+  }
+  
+  ###
+  ###  Write Output 
+  ###
+  
+  keep$tau <- keep$tau/n.mcmc
+  keep$N <- keep$N/(n.mcmc*m)
+  keep$alpha <- keep$alpha/(n.mcmc*m)
+  cat(paste("\nN acceptance rate:",round(keep$N,2)))  
+  cat(paste("\nalpha acceptance rate:",round(keep$alpha,2)))  
+  cat(paste("\ntau acceptance rate:",round(keep$tau,2)))  
+  list(mu.alpha=mu.alpha.save,alpha=alpha.save,N=N.save,lambda=lambda.save,tau=tau.save,keep=keep,n.mcmc=n.mcmc)
+}
---
+++ N.mixture.re.sim.R
@@ -0,0 +1,74 @@
+rm(list=ls())
+
+library(mvtnorm)
+expit <- function(x) 1/(1+exp(-x))
+logit <- function(x) log(x/(1-p))
+
+###
+### Simulate data according to model N.mixture.re.MCMC
+###
+
+m <- 5 # Number of sites
+J <- rpois(m,30) # Number of replicate observations per site; irregular sampling effort
+lambda <- rpois(m,300) # Intensity of Poisson distribution
+N <- rpois(m,lambda) # True population size 
+
+# Create design matrix; pad with NAs to account for irregular sampling effort
+W <- array(NA,dim=c(max(J),2,m))
+for(i in 1:m){
+  W[1:J[i],,i] <- cbind(1,rgamma(J[i],12,2)) # Detection covariate, e.g., flight duration
+}
+hist(W[,2,])
+
+
+# Detection probability
+mu.alpha <- matrix(c(-5,0.75),2,1) # Detection parameters
+tau <- 0.25
+alpha <- rmvnorm(m,mu.alpha,tau^2*diag(length(mu.alpha)))
+curve(expit(cbind(1,x)%*%mu.alpha),xlim=c(0,max(W[,2,],na.rm=TRUE)),add=FALSE)
+for(i in 1:m) curve(expit(cbind(1,x)%*%alpha[i,]),add=TRUE,lty=2,col=i)
+
+p <- matrix(NA,m,max(J),byrow=TRUE) # Detection probability
+for(i in 1:m){
+  p[i,] <- expit(W[,,i]%*%alpha[i,])
+}
+
+# Observed counts; pad with NAs to account for irregular sampling effort
+Y <- matrix(NA,m,max(J))
+for(i in 1:m){
+  Y[i,1:J[i]] <- rbinom(J[i],N[i],p[i,])  
+}
+
+W <- apply(W,2,I) # Flatten array; i.e., convert 3D array to 2D matrix
+
+# Compare observed versus true detection probability
+p.hat <- Y/N # Observed detection probability
+matplot(p.hat,pch=19,cex=1,col=rgb(0,0,0,0.25),xlab="Site")
+matplot(p,pch=19,cex=0.5,col=rgb(1,0,0,0.25),add=TRUE) # True detection probability
+
+# Plot true abundance and observed counts
+plot(N,pch=19,ylim=c(min(Y,na.rm=TRUE),max(N))) # True abundance
+points(rep(1:m,max(J)),Y,cex=(p+1)^2,pch=19,col=rgb(1,0,0,0.25)) # Observed counts
+
+
+###
+### Fit model using MCMC algorithm
+###
+
+source("/Users/bmb/Documents/git/Nmixture/N.mixture.re.MCMC.R")
+# hist(rgamma(1000,5,0.01))
+priors <- list(r=5,q=0.001,zeta=2,a=0,b=5,mu.0=rep(0,2))
+tune <- list(N=15,alpha=0.01,tau=0.25)
+start <- list(N=N,alpha=alpha,lambda=lambda,mu.alpha=mu.alpha,tau=tau)
+out1 <- N.mixture.re.MCMC(Y,W,priors,tune,start,n.mcmc=10000)
+
+matplot(out1$N,type="l",col=1:m,lty=1);abline(h=N,col=1:m,lty=2)
+apply(out1$N,2,quantile,c(0.025,0.975))
+N
+matplot(out1$mu.alpha,type="l",col=1:2);abline(h=mu.alpha,col=1:2,lty=2)
+idx <- 1
+matplot(out1$alpha[,,idx],type="l",col=1:2);abline(h=alpha[idx,],col=1:2,lty=2)
+plot(out1$tau,type="l");abline(h=tau,col=2,lty=2)
+matplot(out1$lambda,type="l",col=1:m,lty=1);abline(h=lambda,col=1:m,lty=2)
+apply(out1$lambda,2,quantile,c(0.025,0.975))
+lambda
