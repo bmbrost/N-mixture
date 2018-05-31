3d9941effc877ed2b3cd205fa52547c2ac3dd0f5
Brian M. Brost <brian.brost@noaa.gov>
2018-05-31 11:40:48 -0700

    Minor changes

--- base.N.mixture.sim.R
+++ base.N.mixture.sim.R
@@ -4,8 +4,8 @@ rm(list=ls())
 ### Simulate data according to model base.N.mixture.MCMC
 ###
 
-m <- 1 # Number of sites
-J <- rep(100,m) # Number of replicate observations
+m <- 20 # Number of sites
+J <- rep(10,m) # Number of replicate observations
 lambda <- rep(150,m) # Intensity of Poisson distribution
 p <- rep(0.75,m) # Detection probability
 N <- rpois(m,lambda) # True population size 
@@ -16,12 +16,12 @@ Y <- t(sapply(1:m,function(x) rbinom(J[x],N[x],p[x]))) # Observed counts
 ### Fit model using MCMC algorithm
 ###
 
-source("/Users/brost/Documents/git/N-mixture/base.N.mixture.MCMC.R")
+source("~/Git/n_mixture/base.N.mixture.MCMC.R")
 # hist(rgamma(1000,2,0.01))
 priors <- list(a=1,b=1,r=2,q=0.01)
-tune <- list(N=2)
+tune <- list(N=6)
 start <- list(N=N,lambda=lambda,p=p)
-out1 <- base.N.mixture.MCMC(Y,priors,tune,start,n.mcmc=50000)
+out1 <- base.N.mixture.MCMC(Y,priors,tune,start,n.mcmc=5000)
 
 matplot(out1$N,type="l");abline(h=N,col=2,lty=2)
 matplot(out1$p,type="l");abline(h=p,col=2,lty=2)
